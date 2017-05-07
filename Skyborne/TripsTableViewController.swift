c53714d41b446875e5c45e93560b85ca45bec969
Dominic Philip <domi.a.philip@gmail.com>
2017-05-06 21:45:10 -0400

    add comments

--- Skyborne/TripsTableViewController.swift
+++ Skyborne/TripsTableViewController.swift
@@ -17,17 +17,17 @@ class TripsTableViewController: UITableViewController {
     // MARK: Table View Data Sources
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1;
+        return 1; // TODO: Remove Stub
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1;
+        return 1; // TODO: Remove Stub
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Trip Cell", for: indexPath)
         
-        cell.textLabel?.text = "New York City"
+        cell.textLabel?.text = "New York City" // TODO: Remove Stub
         
         return cell
     }
