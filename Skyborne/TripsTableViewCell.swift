b3eee7141b64ad9579b9db3dbaf661b04b060e41
Dominic Philip <domi.a.philip@gmail.com>
2017-05-06 22:34:59 -0400

    add trips table view cell schema

---
+++ Skyborne/TripsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TripsTableViewCell.swift
+//  Skyborne
+//
+//  Created by Dominic Philip on 5/6/17.
+//  Copyright © 2017 Skyborne Inc. All rights reserved.
+//
+
+import UIKit
+
+class TripsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
