f0690c449f1ad91fa6cb50cfdddfec63f8d8154b
devericx <devericx@gmail.com>
2017-07-27 15:12:36 -0400

    fetch raw promise from localhost api

--- src/screens/active-trip.js
+++ src/screens/active-trip.js
@@ -1,9 +1,31 @@
 import React from 'react';
-
+import { View, Button } from 'react-native';
 import { Header } from '../components';
 
 const ActiveTrip = () => (
-    <Header>Active Trip</Header>
+
+    <View>
+        <Header>Active Trip</Header>
+        <View style={ styles.buttonStyle }>
+            <Button
+                onPress={ onPress }
+                title='Button'
+                color='#007aff'
+            />
+        </View>
+    </View>
+
 );
+
+const onPress = () => {
+    console.log(fetch('http://localhost:8000/v1/keygen').then((response) => response.json()));
+};
+
+const styles = {
+    buttonStyle: {
+        borderWidth: 1,
+        padding: 5
+    }
+};
 
 export default ActiveTrip;
