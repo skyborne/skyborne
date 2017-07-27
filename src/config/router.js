32a31acfaa55a4fc6467a97394a576a575ad207a
devericx <devericx@gmail.com>
2017-07-27 13:54:31 -0400

    show ios-edge icon in tab bar

--- src/config/router.js
+++ src/config/router.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import { TabNavigator } from 'react-navigation';
+// import Icon from 'react-native-vector-icons/FontAwesome';
 
 import ActiveTrip from '../screens/active-trip';
 import PastTrips from '../screens/past-trips';
 
+import { createIconSetFromFontello } from 'react-native-vector-icons';
+import fontelloConfig from '../../assets/fonts/edge-icon/config.json';
+
+const Icon = createIconSetFromFontello(fontelloConfig);
+
 export const Tabs = TabNavigator({
     PastTrips: {
         screen: PastTrips,
+        navigationOptions: {
+            tabBarIcon: ({tintColor}) => <Icon name='compass' size={30} color={tintColor}/>
+        }
     },
 
     ActiveTrip: {
-        screen: ActiveTrip,
+        screen: ActiveTrip
+    }
+}, {
+    tabBarOptions: {
+        showLabel: false,
+        activeTintColor: '#000',
+        style: {
+            height: 60,
+            borderTopWidth: 0.5,
+            backgroundColor: '#fff'
+        }
     }
 });
