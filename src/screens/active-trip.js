3359351b6d415410ffbca77a5af5b6454570fb53
Dominic Philip <domi.a.philip@gmail.com>
2017-07-27 19:29:40 -0400

    implement button to fetch id on click

--- src/screens/active-trip.js
+++ src/screens/active-trip.js
@@ -1,27 +1,43 @@
-import React from 'react';
-import { View, Button } from 'react-native';
+import React, { Component } from 'react';
+import { Button, Text, View } from 'react-native';
 
 import { Header } from '../components';
 
-const ActiveTrip = () =>
-  <View>
-    <Header>Active Trip</Header>
-    <View style={styles.buttonStyle}>
-      <Button onPress={onPress} title="Button" color="#007aff" />
-    </View>
-  </View>;
+class ActiveTrip extends Component {
+  state = { id: 0 };
 
-const onPress = () => {
-  return fetch('http://localhost:8000/v1/keygen')
-    .then(response => response.text())
-    .then(text => (text.length ? JSON.parse(text) : {}))
-    .then(results =>
-      console.log(results.id);
-    })
-    .catch(error => {
-      console.error(error);
-    });
-};
+  constructor(props) {
+    super(props);
+
+    this.onPress = this.onPress.bind(this);
+  }
+
+  onPress = () => {
+    fetch('http://localhost:8000/v1/keygen')
+      .then(response => response.text())
+      .then(text => (text.length ? JSON.parse(text) : {}))
+      .then(responseJSON => {
+        this.setState({ id: responseJSON.id });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  render() {
+    return (
+      <View>
+        <Header>Active Trip</Header>
+        <Text>
+          {this.state.id}
+        </Text>
+        <View style={styles.buttonStyle}>
+          <Button onPress={this.onPress} title="Button" color="#007aff" />
+        </View>
+      </View>
+    );
+  }
+}
 
 const styles = {
   buttonStyle: {
