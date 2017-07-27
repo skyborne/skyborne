54ee9291f1ec1fba25c1c324226d98dbb55e6519
Dominic Philip <domi.a.philip@gmail.com>
2017-07-26 20:03:32 -0400

    move code to 'skyborne' to access from android and ios

--- index.android.js
+++ index.android.js
@@ -1,11 +1,1 @@
-import React, { Component } from 'react';
-import { View, AppRegistry } from 'react-native';
-
-import { Header } from './src/components';
-import { Tabs } from './src/config/router';
-
-const Skyborne = () => (
-   <Tabs />
-);
-
-AppRegistry.registerComponent('Skyborne', () => Skyborne);
+import './skyborne';
