112ddeaedfc6e8bcef04838d400c25815981f609
Dominic Philip <domi.a.philip@gmail.com>
2017-08-03 18:48:19 -0400

    add animated fade

--- src/components/fluid-card.js
+++ src/components/fluid-card.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { View, Button, TouchableOpacity, Text } from 'react-native';
+import { Animated, View, Button, TouchableOpacity, Text } from 'react-native';
 
-const FluidCard = props => {
-  return (
-    <View style={props.style}>
-      {props.children}
-    </View>
-  );
-};
+export class FluidCard extends React.Component {
+  state = {
+    fade: new Animated.Value(0),
+  };
 
-export { FluidCard };
+  componentWillMount() {
+    Animated.timing(this.state.fade, {
+      toValue: 1,
+      duration: 5000,
+    }).start();
+  }
+
+  render() {
+    let { fade } = this.state;
+
+    return (
+      <Animated.View style={{ ...this.props.style, opacity: fade }}>
+        {this.props.children}
+      </Animated.View>
+    );
+  }
+}
