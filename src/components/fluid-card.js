4d25ac868d774d9a0304a18ef505cf19f8dbce5e
devericx <devericx@gmail.com>
2017-08-05 23:49:54 -0400

    allow extra styles to be applied

--- src/components/fluid-card.js
+++ src/components/fluid-card.js
@@ -4,29 +4,16 @@ import { Animated, View, Button, TouchableOpacity, Text } from 'react-native';
 import { height, width } from '../global';
 
 export class FluidCard extends React.Component {
-  state = {
-    fade: new Animated.Value(0),
-  };
-
-  componentWillMount() {
-    Animated.timing(this.state.fade, {
-      toValue: 1,
-      duration: 2000,
-    }).start();
-  }
-
   render() {
     this.style.height = this.props.height;
-
     return (
-      <Animated.View style={this.style}>
+      <Animated.View style={[this.style, this.props.style]}>
         {this.props.children}
       </Animated.View>
     );
   }
 
   style = {
-    top: height * 0.08,
     width: width * 0.8,
     shadowOffset: { width: 0, height: 0 },
     shadowRadius: 15,
@@ -38,6 +25,5 @@ export class FluidCard extends React.Component {
     backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
-    opacity: this.state.fade,
   };
 }
