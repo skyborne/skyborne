2a4d3984a9b871e5ec6d086e3966f4ee0527014c
devericx <devericx@gmail.com>
2017-08-07 16:39:04 -0400

    move icons to separate file

---
+++ src/resources/icon.js
@@ -0,0 +1,6 @@
+import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
+import icoMoonConfig from '../../assets/fonts/edge-icon/config.json';
+
+const Icon = createIconSetFromIcoMoon(icoMoonConfig);
+
+export default Icon;
