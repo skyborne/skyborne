d66932b8e1e1aa1644ff3182f84b337c5812c9f4
devericx <devericx@gmail.com>
2017-08-09 13:18:45 -0400

    change displayNewTripView to false by default

--- src/screens/ongoing.js
+++ src/screens/ongoing.js
@@ -23,7 +23,7 @@ class Ongoing extends Component {
   state = {
     fade: new Animated.Value(0),
     test_fade: new Animated.Value(0),
-    displayNewTripView: true,
+    displayNewTripView: false,
     blur: false,
     disabled: true,
   };
