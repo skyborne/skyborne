22c31c3d2c1fd747c0af36c2b168516837e4844d
Dominic Philip <domi.a.philip@gmail.com>
2017-08-10 14:41:07 -0400

    new remove item function

--- src/screens/ongoing.js
+++ src/screens/ongoing.js
@@ -26,7 +26,7 @@ class Ongoing extends Component {
   state = {
     onStartFade: new Animated.Value(0),
     newTripFade: new Animated.Value(0),
-    displayNewTripView: true,
+    displayNewTripView: false,
     blur: false,
     id: '',
     results: {},
