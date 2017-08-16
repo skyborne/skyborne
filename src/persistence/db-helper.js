fdfaa601222b8dd886f3d1fea895ad8b284014c1
Dominic Philip <domi.a.philip@gmail.com>
2017-08-15 20:10:55 -0400

    add more helper functions

--- src/persistence/db-helper.js
+++ src/persistence/db-helper.js
@@ -5,10 +5,37 @@ import { TripSchema } from './schema';
 const realm = new Realm({ schema: [TripSchema] });
 
 export function AddTrip(properties) {
-  realm.create('Trip', properties);
+  try {
+    realm.write(() => {
+      realm.create('Trip', properties);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-export function ClearTrips() {
+export function GetTrip(id) {
+  let trip = realm.objects('Trip').filtered('id = ' + id);
+  return trip;
+}
+
+export function GetTrips() {
+  let trips = realm.objects('Trip');
+  return trips;
+}
+
+export function RemoveTrip(id) {
+  try {
+    realm.write(() => {
+      let trip = realm.objects('Trip').filtered('id = ' + id);
+      realm.delete(trip);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export function RemoveTrips() {
   try {
     realm.write(() => {
       let trips = realm.objects('Trip');
